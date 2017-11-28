e82461aaaef50e76555daa54b73c158201da211b
Jason Rosso <jasonrosso@gmail.com>
2017-11-28 14:22:40 -0500

    added DATABASE_URL in line 8 of db index.js

--- db/index.js
+++ db/index.js
@@ -5,7 +5,7 @@ const Sequelize = require('sequelize');
 const pkg = require('../package.json');
 
 const name = process.env.DATABASE_NAME || pkg.name;
-const connectionString = process.env.DATABASE_connectionString || `postgres://localhost:5432/${pkg.name}`;
+const connectionString = process.env.DATABASE_connectionString || `postgres://localhost:5432/${pkg.name}` || process.env.DATABASE_URL;
 
 console.log(chalk.yellow(`Opening database connection to ${connectionString}`));
 
